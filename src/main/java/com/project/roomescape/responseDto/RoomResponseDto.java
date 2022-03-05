3b7a1bc2bf5fe71332174bfebf761a664fb80021
Kyuwon Choi <chl1465@naver.com>
2022-03-05 18:01:04 +0900

    최규원url추가

--- src/main/java/com/project/roomescape/responseDto/RoomResponseDto.java
+++ src/main/java/com/project/roomescape/responseDto/RoomResponseDto.java
@@ -1,6 +1,8 @@
 package com.project.roomescape.responseDto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
@@ -11,14 +13,17 @@ public class RoomResponseDto {
     private Long count;
     private String createdUser;
     private Integer currentNum;    // 현재 참여 인원
+    private String url;
 
-    public RoomResponseDto(Long roomId, String teamName, Long count, String createdUser, Integer currentNum) {
+    public RoomResponseDto(Long roomId, String teamName, Long count, String createdUser, Integer currentNum, String url) {
         this.roomId = roomId;
         this.teamName = teamName;
         this.count = count;
         this.createdUser = createdUser;
         this.currentNum = currentNum;
+        this.url = url;
     }
+
 
 
 }
