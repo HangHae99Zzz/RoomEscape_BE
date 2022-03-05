804d51fdaddfc6e03ef4f2d636b44f6c7ce47ded
Wonjae <wonjae0709@naver.com>
2022-03-05 17:30:22 +0900

    https 적용완료

--- src/main/java/com/project/roomescape/config/WebConfig.java
+++ src/main/java/com/project/roomescape/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://127.0.0.1:5500", "http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization")
