e5710af1f38f1d3955a509b7be1046bf73d7138f
Wonjae <wonjae0709@naver.com>
2022-03-16 12:35:08 +0900

    temp

--- src/main/java/com/project/roomescape/config/RedisConfig.java
+++ src/main/java/com/project/roomescape/config/RedisConfig.java
@@ -1,12 +1,13 @@
 package com.project.roomescape.config;
 
-import io.lettuce.core.dynamic.annotation.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.stereotype.Component;
 
 @Configuration
 @PropertySource("classpath:application-aws.properties")
@@ -20,6 +21,12 @@ public class RedisConfig {
 
     @Value("${spring.redis.password}")
     private String redisPassword;
+
+    //You need this 이게 있어야 @propertySource가 작동한다.
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyConfigInDev() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
