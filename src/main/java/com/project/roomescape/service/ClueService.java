d09d642da4cfe1c85816a6e1c820570a57de1788
paran21 <knospe1@naver.com>
2022-03-22 20:06:22 +0900

    Feat: Clue와 Room의 연관관계 끊기
    Clue entity의 Room 삭제하고 roomId 추가
    Clue 생성하기 수정 : roomId만 저장
    
    Resolves: #60

--- src/main/java/com/project/roomescape/service/ClueService.java
+++ src/main/java/com/project/roomescape/service/ClueService.java
@@ -1,11 +1,7 @@
 package com.project.roomescape.service;
 
-import com.project.roomescape.exception.CustomException;
-import com.project.roomescape.exception.ErrorCode;
 import com.project.roomescape.model.Clue;
-import com.project.roomescape.model.Room;
 import com.project.roomescape.repository.ClueRepository;
-import com.project.roomescape.repository.RoomRepository;
 import com.project.roomescape.responseDto.ClueResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,22 +16,19 @@ import java.util.Random;
 public class ClueService {
 
     private final ClueRepository clueRepository;
-    private final RoomRepository roomRepository;
 
     // clue 생성하기
     @Transactional
     public void createClue(Long roomId) {
         Random random = new Random();
-        Room room = roomRepository.findById(roomId)
-                .orElseThrow(()-> new CustomException(ErrorCode.ROOM_NOT_FOUND));
 
         // clue 생성해서 List에 담기
         List<Clue> clueList = new ArrayList<>();
         String[] arr = new String[]{"+", "-"};
 
-        clueList.add(new Clue(room, "Ba1", String.valueOf(random.nextInt(3999) + 1000)));
-        clueList.add(new Clue(room, "Ba2", String.valueOf(random.nextInt(3999) + 1000)));
-        clueList.add(new Clue(room, "Ba3", arr[random.nextInt(1)]));
+        clueList.add(new Clue(roomId, "Ba1", String.valueOf(random.nextInt(3999) + 1000)));
+        clueList.add(new Clue(roomId, "Ba2", String.valueOf(random.nextInt(3999) + 1000)));
+        clueList.add(new Clue(roomId, "Ba3", arr[random.nextInt(1)]));
 
         // List 저장하기
         clueRepository.saveAll(clueList);
