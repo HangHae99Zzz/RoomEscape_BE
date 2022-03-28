8e4880f1e64c897d01909369566b75d9f29a1bc3
Wonjae <wonjae0709@naver.com>
2022-03-28 14:10:07 +0900

    Feat:통합 테스트코드, 단위 테스트 코드 추가 && Fix:랭킹 조회시 랭킹 계산 버그 수정
    통합테스트코드 모든 Controller에 대해서 작성 완료.
    퀴즈 생성 단위 테스트 코드(시간측정) 추가
    랭킹 조회시 랭킹 계산 오류 수정 완료.
    
    Resolves: #121, #82

--- src/main/java/com/project/roomescape/requestDto/RoomRequestDto.java
+++ src/main/java/com/project/roomescape/requestDto/RoomRequestDto.java
@@ -2,9 +2,11 @@ package com.project.roomescape.requestDto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class RoomRequestDto {
     private String teamName;
     private String userId;
