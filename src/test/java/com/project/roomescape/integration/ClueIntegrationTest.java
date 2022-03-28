8fc65ce0d1dcced3d6f73c91e3d11677126d4c15
Wonjae <wonjae0709@naver.com>
2022-03-28 16:26:38 +0900

    Feat:build시 테스트코드 실행 안되게 설정 추가
    테스트 코드 build시에 자동으로 실행되던 것을 설정 변경으로 막음.

--- src/test/java/com/project/roomescape/integration/ClueIntegrationTest.java
+++ src/test/java/com/project/roomescape/integration/ClueIntegrationTest.java
@@ -1,102 +1,102 @@
-//package com.project.roomescape.integration;
-//
-//import com.project.roomescape.model.Clue;
-//import com.project.roomescape.model.Room;
-//import com.project.roomescape.repository.ClueRepository;
-//import com.project.roomescape.repository.RoomRepository;
-//import org.junit.jupiter.api.*;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.web.reactive.server.WebTestClient;
-//
-//import java.util.Optional;
-//
-//import static org.mockito.Mockito.when;
-//
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = {"spring.config.location=classpath:application-test.properties"})
-//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-//@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-//public class ClueIntegrationTest {
-//
-//    @Autowired
-//    private WebTestClient webTestClient;
-//
-//    @Autowired
-//    private ClueRepository clueRepository;
-//
-//    @MockBean
-//    private RoomRepository mockRoomRepository;
-//
-//    //게임시작해야 clue가 만들어지기 때문에 게임시작하기부터 실행한다.
-//    @Test
-//    @Order(1)
-//    @DisplayName("게임시작하기")
-//    void startGame(){
-//        Room room = new Room("테스트팀", "테스트유저ID");
-//
-//        when(mockRoomRepository.findById(1L)).thenReturn(Optional.of(room));
-//
-//
-//        webTestClient.put().uri("/games/{roomId}", 1)
-//                .exchange()
-//                .expectStatus().isOk();
-//    }
-//
-//    @Test
-//    @Order(2)
-//    @DisplayName("Ba1 clue 조회하기")
-//    void getBa1Clue(){
-//        Clue clue = clueRepository.findByRoomIdAndType(1L, "Ba1");
-//
-//        webTestClient.get().uri("/rooms/{roomId}/clues/{clueType}", 1, "Ba1")
-//                .exchange()
-//                .expectStatus().isOk()
-//                .expectHeader().contentType(MediaType.APPLICATION_JSON)
-//                .expectBody()
-//                .jsonPath("$.content").isEqualTo(clue.getContent());
-//    }
-//
-//    @Test
-//    @Order(3)
-//    @DisplayName("Ba2 clue 조회하기")
-//    void getBa2Clue(){
-//        Clue clue = clueRepository.findByRoomIdAndType(1L, "Ba2");
-//
-//        webTestClient.get().uri("/rooms/{roomId}/clues/{clueType}", 1, "Ba2")
-//                .exchange()
-//                .expectStatus().isOk()
-//                .expectHeader().contentType(MediaType.APPLICATION_JSON)
-//                .expectBody()
-//                .jsonPath("$.content").isEqualTo(clue.getContent());
-//    }
-//
-//    @Test
-//    @Order(4)
-//    @DisplayName("Ba3 clue 조회하기")
-//    void getBa3Clue(){
-//        Clue clue = clueRepository.findByRoomIdAndType(1L, "Ba3");
-//
-//        webTestClient.get().uri("/rooms/{roomId}/clues/{clueType}", 1, "Ba3")
-//                .exchange()
-//                .expectStatus().isOk()
-//                .expectHeader().contentType(MediaType.APPLICATION_JSON)
-//                .expectBody()
-//                .jsonPath("$.content").isEqualTo(clue.getContent());
-//    }
-//
-//    @Test
-//    @Order(5)
-//    @DisplayName("DB에 존재하지 않는 clue 조회하기")
-//    void getClue(){
-//
-//        webTestClient.get().uri("/rooms/{roomId}/clues/{clueType}", 1, "AA")
-//                .exchange()
-//                .expectStatus().isOk()
-//                .expectHeader().contentType(MediaType.APPLICATION_JSON)
-//                .expectBody()
-//                .jsonPath("$.content").isEqualTo("");
-//    }
-//
-//}
+package com.project.roomescape.integration;
+
+import com.project.roomescape.model.Clue;
+import com.project.roomescape.model.Room;
+import com.project.roomescape.repository.ClueRepository;
+import com.project.roomescape.repository.RoomRepository;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = {"spring.config.location=classpath:application-test.properties"})
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class ClueIntegrationTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Autowired
+    private ClueRepository clueRepository;
+
+    @MockBean
+    private RoomRepository mockRoomRepository;
+
+    //게임시작해야 clue가 만들어지기 때문에 게임시작하기부터 실행한다.
+    @Test
+    @Order(1)
+    @DisplayName("게임시작하기")
+    void startGame(){
+        Room room = new Room("테스트팀", "테스트유저ID");
+
+        when(mockRoomRepository.findById(1L)).thenReturn(Optional.of(room));
+
+
+        webTestClient.put().uri("/games/{roomId}", 1)
+                .exchange()
+                .expectStatus().isOk();
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("Ba1 clue 조회하기")
+    void getBa1Clue(){
+        Clue clue = clueRepository.findByRoomIdAndType(1L, "Ba1");
+
+        webTestClient.get().uri("/rooms/{roomId}/clues/{clueType}", 1, "Ba1")
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$.content").isEqualTo(clue.getContent());
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Ba2 clue 조회하기")
+    void getBa2Clue(){
+        Clue clue = clueRepository.findByRoomIdAndType(1L, "Ba2");
+
+        webTestClient.get().uri("/rooms/{roomId}/clues/{clueType}", 1, "Ba2")
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$.content").isEqualTo(clue.getContent());
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Ba3 clue 조회하기")
+    void getBa3Clue(){
+        Clue clue = clueRepository.findByRoomIdAndType(1L, "Ba3");
+
+        webTestClient.get().uri("/rooms/{roomId}/clues/{clueType}", 1, "Ba3")
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$.content").isEqualTo(clue.getContent());
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("DB에 존재하지 않는 clue 조회하기")
+    void getClue(){
+
+        webTestClient.get().uri("/rooms/{roomId}/clues/{clueType}", 1, "AA")
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$.content").isEqualTo("");
+    }
+
+}
--- src/test/java/com/project/roomescape/integration/quizintegrationtest/QuizAaIntegrationTest.java
+++ src/test/java/com/project/roomescape/integration/quizintegrationtest/QuizAaIntegrationTest.java
@@ -1,148 +1,148 @@
-//package com.project.roomescape.integration.quizintegrationtest;
-//
-//import com.project.roomescape.model.GameResource;
-//import com.project.roomescape.model.Pass;
-//import com.project.roomescape.model.Quiz;
-//import com.project.roomescape.model.Room;
-//import com.project.roomescape.repository.GameResourceRepository;
-//import com.project.roomescape.repository.QuizRepository;
-//import com.project.roomescape.repository.RoomRepository;
-//import com.project.roomescape.requestDto.RoomRequestDto;
-//import com.project.roomescape.responseDto.QuizResponseDto;
-//import org.junit.jupiter.api.*;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.http.*;
-//import org.springframework.test.web.reactive.server.WebTestClient;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.Optional;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.mockito.Mockito.when;
-//
-//
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = {"spring.config.location=classpath:application-test.properties"})
-//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-//@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-//public class QuizAaIntegrationTest {
-//
-//    @Autowired
-//    private WebTestClient webTestClient;
-//
-//    @Autowired
-//    private QuizRepository quizRepository;
-//
-//    @Autowired
-//    private RoomRepository roomRepository;
-//
-//    @MockBean
-//    private GameResourceRepository mockGameResourceRepository;
-//
-//    //방 개설하기부터 할 수 밖에 없습니다. 왜냐하면 퀴즈 생성할때 findbyRoomAndType이 있는데 이 로직은 무조건 룸이 먼저
-//    //저장되고 나서야 실행될 수 있는 로직이기 때문에 Room을 mock으로 처리할 수 없습니다.
-//    @Test
-//    @Order(1)
-//    @DisplayName("방 개설하기")
-//    void createRoom(){
-//
-//        String teamName = "테스트팀";
-//        String userId = "테스트유저ID";
-//        RoomRequestDto roomRequestDto = new RoomRequestDto(teamName, userId);
-//
-//        GameResource gameResource1 = new GameResource("userImg", "임시url1");
-//        GameResource gameResource2 = new GameResource("userImg", "임시url2");
-//        GameResource gameResource3 = new GameResource("userImg", "임시url3");
-//        GameResource gameResource4 = new GameResource("userImg", "임시url4");
-//        List<GameResource> mockGameResourceList = new ArrayList<>();
-//        mockGameResourceList.add(gameResource1);
-//        mockGameResourceList.add(gameResource2);
-//        mockGameResourceList.add(gameResource3);
-//        mockGameResourceList.add(gameResource4);
-//
-//        when(mockGameResourceRepository.findAllByType(gameResource1.getType())).thenReturn(mockGameResourceList);
-//
-//        // when
-//        webTestClient.post().uri("/rooms")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .bodyValue(roomRequestDto)
-//                .exchange()
-//                .expectStatus().isOk()
-//                .expectHeader().contentType(MediaType.APPLICATION_JSON)
-//                .expectBody()
-//                .jsonPath("$.roomId").isEqualTo(1L)
-//                .jsonPath("$.teamName").isEqualTo("테스트팀")
-//                .jsonPath("$.createdUser").isEqualTo("테스트유저ID")
-//                .jsonPath("$.currentNum").isEqualTo(1)
-//                .jsonPath("$.url").isEqualTo("/room/1")
-//                .jsonPath("$.userList").isNotEmpty()
-//                .jsonPath("$.startAt").isEmpty();
-//    }
-//
-//
-//    @Test
-//    @Order(2)
-//    @DisplayName("퀴즈 Aa 생성하기")
-//    void getQuizAa(){
-//        // when
-//        webTestClient.get().uri("/rooms/{roomId}/quizzes/{quizType}", 1, "Aa")
-//                .exchange()
-//                .expectStatus().isOk()
-//                .expectHeader().contentType(MediaType.APPLICATION_JSON)
-//                .expectBody()
-//                .jsonPath("$.question").isEqualTo("지금 몇시지?")
-//                .jsonPath("$.content").isNotEmpty()
-//                .jsonPath("$.hint").isEmpty()
-//                .jsonPath("$.chance").isEqualTo("시계를 돌려볼까?")
-//                .jsonPath("$.answer").isNotEmpty()
-//                .jsonPath("$.pass").isEqualTo("FAIL");
-//
-//    }
-//
-//    @Test
-//    @Order(3)
-//    @DisplayName("퀴즈 Aa 불러오기")
-//    void getSameQuizAa(){
-//        Optional<Room> room = roomRepository.findById(1L);
-//        Optional<Quiz> temporary = quizRepository.findByRoomAndType(room.get(), "Aa");
-//        QuizResponseDto quizResponseDto = new QuizResponseDto();
-//        if(temporary.isPresent()) {
-//            Quiz quiz = temporary.get();
-//            quizResponseDto = new QuizResponseDto(
-//                    quiz.getQuestion(), quiz.getContent(), quiz.getHint(),
-//                    quiz.getChance(), quiz.getAnswer(), quiz.getPass());
-//        }
-//
-//        webTestClient.get().uri("/rooms/{roomId}/quizzes/{quizType}", 1, "Aa")
-//                .exchange()
-//                .expectStatus().isOk()
-//                .expectHeader().contentType(MediaType.APPLICATION_JSON)
-//                .expectBody()
-//                .jsonPath("$.question").isEqualTo(quizResponseDto.getQuestion())
-//                .jsonPath("$.content").isEqualTo(quizResponseDto.getContent())
-//                .jsonPath("$.hint").isEqualTo(quizResponseDto.getHint())
-//                .jsonPath("$.chance").isEqualTo(quizResponseDto.getChance())
-//                .jsonPath("$.answer").isEqualTo(quizResponseDto.getAnswer())
-//                .jsonPath("$.pass").isEqualTo("FAIL");
-//    }
-//
-//    @Test
-//    @Order(4)
-//    @DisplayName("퀴즈 완료시키기")
-//    void finishedQuiz() {
-//
-//        webTestClient.put().uri("/rooms/{roomId}/quizzes/{quizType}", 1, "Aa")
-//                .exchange()
-//                .expectStatus().isOk();
-//
-//        Optional<Room> room = roomRepository.findById(1L);
-//        Optional<Quiz> quiz = quizRepository.findByRoomAndType(room.get(),"Aa");
-//
-//        assertEquals(quiz.get().getPass(), Pass.SUCCESS);
-//
-//    }
-//
-//
-//}
+package com.project.roomescape.integration.quizintegrationtest;
+
+import com.project.roomescape.model.GameResource;
+import com.project.roomescape.model.Pass;
+import com.project.roomescape.model.Quiz;
+import com.project.roomescape.model.Room;
+import com.project.roomescape.repository.GameResourceRepository;
+import com.project.roomescape.repository.QuizRepository;
+import com.project.roomescape.repository.RoomRepository;
+import com.project.roomescape.requestDto.RoomRequestDto;
+import com.project.roomescape.responseDto.QuizResponseDto;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.*;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = {"spring.config.location=classpath:application-test.properties"})
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class QuizAaIntegrationTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Autowired
+    private QuizRepository quizRepository;
+
+    @Autowired
+    private RoomRepository roomRepository;
+
+    @MockBean
+    private GameResourceRepository mockGameResourceRepository;
+
+    //방 개설하기부터 할 수 밖에 없습니다. 왜냐하면 퀴즈 생성할때 findbyRoomAndType이 있는데 이 로직은 무조건 룸이 먼저
+    //저장되고 나서야 실행될 수 있는 로직이기 때문에 Room을 mock으로 처리할 수 없습니다.
+    @Test
+    @Order(1)
+    @DisplayName("방 개설하기")
+    void createRoom(){
+
+        String teamName = "테스트팀";
+        String userId = "테스트유저ID";
+        RoomRequestDto roomRequestDto = new RoomRequestDto(teamName, userId);
+
+        GameResource gameResource1 = new GameResource("userImg", "임시url1");
+        GameResource gameResource2 = new GameResource("userImg", "임시url2");
+        GameResource gameResource3 = new GameResource("userImg", "임시url3");
+        GameResource gameResource4 = new GameResource("userImg", "임시url4");
+        List<GameResource> mockGameResourceList = new ArrayList<>();
+        mockGameResourceList.add(gameResource1);
+        mockGameResourceList.add(gameResource2);
+        mockGameResourceList.add(gameResource3);
+        mockGameResourceList.add(gameResource4);
+
+        when(mockGameResourceRepository.findAllByType(gameResource1.getType())).thenReturn(mockGameResourceList);
+
+        // when
+        webTestClient.post().uri("/rooms")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(roomRequestDto)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$.roomId").isEqualTo(1L)
+                .jsonPath("$.teamName").isEqualTo("테스트팀")
+                .jsonPath("$.createdUser").isEqualTo("테스트유저ID")
+                .jsonPath("$.currentNum").isEqualTo(1)
+                .jsonPath("$.url").isEqualTo("/room/1")
+                .jsonPath("$.userList").isNotEmpty()
+                .jsonPath("$.startAt").isEmpty();
+    }
+
+
+    @Test
+    @Order(2)
+    @DisplayName("퀴즈 Aa 생성하기")
+    void getQuizAa(){
+        // when
+        webTestClient.get().uri("/rooms/{roomId}/quizzes/{quizType}", 1, "Aa")
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$.question").isEqualTo("지금 몇시지?")
+                .jsonPath("$.content").isNotEmpty()
+                .jsonPath("$.hint").isEmpty()
+                .jsonPath("$.chance").isEqualTo("시계를 돌려볼까?")
+                .jsonPath("$.answer").isNotEmpty()
+                .jsonPath("$.pass").isEqualTo("FAIL");
+
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("퀴즈 Aa 불러오기")
+    void getSameQuizAa(){
+        Optional<Room> room = roomRepository.findById(1L);
+        Optional<Quiz> temporary = quizRepository.findByRoomAndType(room.get(), "Aa");
+        QuizResponseDto quizResponseDto = new QuizResponseDto();
+        if(temporary.isPresent()) {
+            Quiz quiz = temporary.get();
+            quizResponseDto = new QuizResponseDto(
+                    quiz.getQuestion(), quiz.getContent(), quiz.getHint(),
+                    quiz.getChance(), quiz.getAnswer(), quiz.getPass());
+        }
+
+        webTestClient.get().uri("/rooms/{roomId}/quizzes/{quizType}", 1, "Aa")
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$.question").isEqualTo(quizResponseDto.getQuestion())
+                .jsonPath("$.content").isEqualTo(quizResponseDto.getContent())
+                .jsonPath("$.hint").isEqualTo(quizResponseDto.getHint())
+                .jsonPath("$.chance").isEqualTo(quizResponseDto.getChance())
+                .jsonPath("$.answer").isEqualTo(quizResponseDto.getAnswer())
+                .jsonPath("$.pass").isEqualTo("FAIL");
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("퀴즈 완료시키기")
+    void finishedQuiz() {
+
+        webTestClient.put().uri("/rooms/{roomId}/quizzes/{quizType}", 1, "Aa")
+                .exchange()
+                .expectStatus().isOk();
+
+        Optional<Room> room = roomRepository.findById(1L);
+        Optional<Quiz> quiz = quizRepository.findByRoomAndType(room.get(),"Aa");
+
+        assertEquals(quiz.get().getPass(), Pass.SUCCESS);
+
+    }
+
+
+}
