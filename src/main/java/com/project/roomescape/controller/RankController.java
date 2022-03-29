e2debed75ee0beea4e7b2c3b94f2427a0d1a3ea5
Kyuwon Choi <chl1465@naver.com>
2022-03-29 13:08:57 +0900

    Fix: nginx 서버 재가동
    ec2 서버가 꺼져서 다시 가동
    
    Resolve

--- src/main/java/com/project/roomescape/controller/RankController.java
+++ src/main/java/com/project/roomescape/controller/RankController.java
@@ -19,7 +19,7 @@ public class RankController {
     @ApiOperation(value = "전체 랭킹 조회하기")
     @GetMapping("/ranks")
     public List<RankResponseDto> getRanks() {
-        //  roomId에 null이면 오류떠서 -1 이나 0 넣어서 선언하면된다. 값에 -1을 주던 0을 주던 getRanks()에서 어떠한 영향을 주지 못한다.
+        //  roomId에 null이면 오류떠서 -1 이나 0 넣어서 선언하면된다. 값에 -1을 주던 0을 주던 getRanks()에서 어떠한 영향을 주지 못한다.//
         Long roomId = -1L;
         //  밑에 꺼랑 같은 getRanks를 쓰기위해서 roomId를 넣어주는거다
         return rankService.getRanks(roomId);
