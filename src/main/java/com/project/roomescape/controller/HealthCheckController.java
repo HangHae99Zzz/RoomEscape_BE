07f6d8c033b155037d7f65805ae17d68fb7467d6
Kyuwon Choi <chl1465@naver.com>
2022-04-07 18:28:22 +0900

    Comment: 수정

--- src/main/java/com/project/roomescape/controller/HealthCheckController.java
+++ src/main/java/com/project/roomescape/controller/HealthCheckController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthCheckController {
 
-    // Nginx health check용 api
+    // Nginx health check용 api//
     @GetMapping("/health")
     public String checkHealth() {
         return "healthy"; }
