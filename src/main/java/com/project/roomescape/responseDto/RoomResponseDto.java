4c38053859f798451f64267985af83a34ac8e03a
Kyuwon Choi <chl1465@naver.com>
2022-04-15 00:51:22 +0900

    TEST

--- src/main/java/com/project/roomescape/responseDto/RoomResponseDto.java
+++ src/main/java/com/project/roomescape/responseDto/RoomResponseDto.java
@@ -11,7 +11,7 @@ public class RoomResponseDto {
     private Long roomId;
     private String teamName;
     private String createdUser;
-    // 현재 참여 인원
+    //현재 참여 인원
     private int currentNum;
     private String url;
     private List<UserResponseDto> userList;
